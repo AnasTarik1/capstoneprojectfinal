b3ed78105c00461398249f3ea5ac1b4be27f18df
Aaron_Wang <113875829+wychosenone@users.noreply.github.com>
2024-06-27 19:21:38 -0700

    api curd completed

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -23,12 +23,24 @@ from rest_framework.authtoken.views import obtain_auth_token
 router = DefaultRouter()
 router.register(r'tables', views.BookingViewSet)
 
+# urlpatterns = [
+#    path('admin/', admin.site.urls),
+#    path('restaurant/', include('restaurant.urls')),
+#    path('api/', include('restaurant.urls')),
+#    path('restaurant/menu/',include('restaurant.urls')),
+#    path('restaurant/booking/', include(router.urls)),
+#    path('auth/', include('djoser.urls')),
+#    path('auth/', include('djoser.urls.authtoken')),
+#    path('api-token-auth/',obtain_auth_token)
+# ]
+
 urlpatterns = [
    path('admin/', admin.site.urls),
    path('restaurant/', include('restaurant.urls')),
+   path('api/', include('restaurant.urls')),
    path('restaurant/menu/',include('restaurant.urls')),
    path('restaurant/booking/', include(router.urls)),
    path('auth/', include('djoser.urls')),
    path('auth/', include('djoser.urls.authtoken')),
-   path('api-token-auth/',obtain_auth_token,name='api_token_auth')
+   path('api-token-auth/', obtain_auth_token)
 ]
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,10 +1,10 @@
-#define URL route for index() view
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('menu/items', views.MenuItemListView.as_view()),
-    path('restaurant/menu/items/create/', views.MenuItemCreateView.as_view()),
-    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('menu-items/', views.MenuItemListView.as_view(), name='menu-items'),
+    path('menu-items/create/', views.MenuItemCreateView.as_view(), name='menu-item-create'),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view(), name='menu-item-detail'),
 ]  
